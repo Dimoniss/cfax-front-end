e926cc3b5c08f16b97e625c39ce4552d5c9b149f
Dmytro Hryn <dmytro.hryn@sigemi.it>
2024-03-07 17:16:46 +0100

    add router dependence, update lofin form

--- src/components/AuthFormLogin.vue
+++ src/components/AuthFormLogin.vue
@@ -1,14 +1,22 @@
 <template>
-  <div
+  <form
     class="flex-col flex gap-2 border border-slate-500 rounded-xl shadow px-2 py-1 bg-gray-400 h-40 w-56"
   >
     <div class="flex items-center gap-2">
       <img src="/icon/user_icon.png" alt="User" class="w-7" />
-      <h1>Login</h1>
+      <h3>Login</h3>
     </div>
     <div class="flex-col flex gap-2">
-      <input type="email" class="border border-slate-500 rounded-md" />
-      <input type="password" class="border border-slate-500 rounded-md" />
+      <input
+        type="email"
+        class="border border-slate-500 rounded-md form-control pl-2"
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        class="border border-slate-500 rounded-md form-control pl-2"
+        placeholder="Password"
+      />
     </div>
     <div class="flex justify-between">
       <div class="flex flex-col">
@@ -18,8 +26,12 @@
         >
       </div>
       <div>
-        <button class="bg-blue-400 text-white rounded-md px-2 py-1 hover:bg-blue-500">Login</button>
+        <button
+          class="bg-blue-400 text-white rounded-md px-2 py-1 hover:bg-blue-500 btn btn-primary btn-block"
+        >
+          Login
+        </button>
       </div>
     </div>
-  </div>
+  </form>
 </template>
